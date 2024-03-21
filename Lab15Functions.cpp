27477f249e6380cb43876aeaeed76b5764c41d9a
kailawa <156133594+kailawa@users.noreply.github.com>
2024-03-20 22:44:35 -0400

    Update Lab15Functions.cpp

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -3,8 +3,35 @@
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 *******************************************************************/
-void getInfo(int & pickFrom, int & numPicks){
+#include <iostream>
+#include <limits>
+using namespace std;
+void getInfo(int&, int&);
+double computeWays(int, int);
+double factorial(int);
 
+void getInfo(int & pickFrom, int & numPicks){
+    cout << "How many balls (1-12) are in the pool to pick from? ";
+    cin >> pickFrom;
+
+    while (cin.fail() || pickFrom < 1 || pickFrom > 12) {
+        cin.clear(); // Clear the error state
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+        cout << "Input Error! There must be between 1 and 12 balls." << endl;
+        cout << "How many balls (1-12) are in the pool to pick from? ";
+        cin >> pickFrom;
+    }
+
+    cout << "How many balls (1-" << pickFrom << ") will be drawn? ";
+    cin >> numPicks;
+
+    while (numPicks < 1 || numPicks > pickFrom || cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Input Error! " << endl;
+        cout << "How many balls (1-" << pickFrom << ") will be drawn? ";
+        cin >> numPicks;
+    }
 }
 
 /******************************************************************
@@ -19,10 +46,15 @@ void getInfo(int & pickFrom, int & numPicks){
 // multiplying two factorials together. This is done to prevent any
 // intermediate result becoming so large that it causes overflow.
 double computeWays(int n, int k){
-    
+    double ways = (factorial(k) * factorial(n - k)) / factorial(n);
+    return ways;
 }
+
 
 // This function computes factorials recursively. It is called by computeWays.
 double factorial(int n){
-
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
 }
